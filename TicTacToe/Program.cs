2954c10e5a9da06ba1d63bb2f7d7fbcddfc37300
Piotr Bąk <piotr.andrzej.bak@gmail.com>
2022-03-27 01:00:36 +0100

    r0.02

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -6,9 +6,8 @@ namespace TicTacToe
     {
         static void Main(string[] args)
         {
-            Console.ForegroundColor = ConsoleColor.White;
+            Console.ForegroundColor = ConsoleColor.Black;
             Console.BackgroundColor = ConsoleColor.DarkGray;
-            Console.Clear();
 
             string[,] gameArea = new string[3, 3]
              {
@@ -16,18 +15,20 @@ namespace TicTacToe
                     {"4","5","6"},
                     {"7","8","9"}
              };
-            //petla glowna
+            
             while (true)
             {
-                ShowGameArea(gameArea);
-                userPlay(gameArea, 1);
-                ShowGameArea(gameArea);
-                userPlay(gameArea, 2);
+                UserPlay(gameArea, 1);
+                WinEvaluation(gameArea, 1);
+                UserPlay(gameArea, 2);
+                WinEvaluation(gameArea, 2);
             }
         }
 
-        static void userPlay(string[,] gameArea, int playerNumber)
+        static void UserPlay(string[,] gameArea, int playerNumber)
         {
+            Console.Clear();
+            ShowGameArea(gameArea);
             Console.Write($"Player {playerNumber} move: ");
             string userInputString = Console.ReadLine();
             try
@@ -35,7 +36,7 @@ namespace TicTacToe
                 int userInputInt = int.Parse(userInputString);
                 if (userInputInt >= 1 && userInputInt <= 9)
                 {
-                    fieldUpdate(gameArea, playerNumber, userInputInt);
+                    FieldUpdate(gameArea, playerNumber, userInputInt);
                     Console.Clear();
                 }
                 else
@@ -44,7 +45,7 @@ namespace TicTacToe
                     Console.ReadKey();
                     Console.Clear();
                     ShowGameArea(gameArea);
-                    userPlay(gameArea, playerNumber);
+                    UserPlay(gameArea, playerNumber);
 
                 }
             }
@@ -54,7 +55,7 @@ namespace TicTacToe
                 Console.ReadKey();
                 Console.Clear();
                 ShowGameArea(gameArea);
-                userPlay(gameArea, playerNumber);
+                UserPlay(gameArea, playerNumber);
             }
         }
 
@@ -70,7 +71,7 @@ namespace TicTacToe
             }
         }
 
-        static string[,] fieldUpdate(string[,] gameArea, int playerNumber, int fieldNumber)
+        static string[,] FieldUpdate(string[,] gameArea, int playerNumber, int fieldNumber)
         {
             if (fieldNumber <= 9 && fieldNumber >= 1)
             {
@@ -79,91 +80,255 @@ namespace TicTacToe
                     case 1:
                         if (playerNumber == 1)
                         {
-                            gameArea[0, 0] = "O";
+                            if (gameArea[0, 0] == "1")
+                            {
+                                gameArea[0, 0] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                Console.Clear();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[0, 0] = "X";
+                            if (gameArea[0, 0] == "1")
+                            {
+                                gameArea[0, 0] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                Console.Clear();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 2:
                         if (playerNumber == 1)
                         {
-                            gameArea[0, 1] = "O";
+                            if (gameArea[0, 1] == "2")
+                            {
+                                gameArea[0, 1] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[0, 1] = "X";
+                            if (gameArea[0, 1] == "2")
+                            {
+                                gameArea[0, 1] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 3:
                         if (playerNumber == 1)
                         {
-                            gameArea[0, 2] = "O";
+                            if (gameArea[0, 2] == "3")
+                            {
+                                gameArea[0, 2] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[0, 2] = "X";
+                            if (gameArea[0, 2] == "3")
+                            {
+                                gameArea[0, 2] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 4:
                         if (playerNumber == 1)
                         {
-                            gameArea[1, 0] = "O";
+                            if (gameArea[1, 0] == "4")
+                            {
+                                gameArea[1, 0] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[1, 0] = "X";
+                            if (gameArea[1, 0] == "4")
+                            {
+                                gameArea[1, 0] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 5:
                         if (playerNumber == 1)
                         {
-                            gameArea[1, 1] = "O";
+                            if (gameArea[1, 1] == "5")
+                            {
+                                gameArea[1, 1] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[1, 1] = "X";
+                            if (gameArea[1, 1] == "5")
+                            {
+                                gameArea[1, 1] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 6:
                         if (playerNumber == 1)
                         {
-                            gameArea[1, 2] = "O";
+                            if (gameArea[1, 2] == "6")
+                            {
+                                gameArea[1, 2] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[1, 2] = "X";
+                            if (gameArea[1, 2] == "6")
+                            {
+                                gameArea[1, 2] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 7:
                         if (playerNumber == 1)
                         {
-                            gameArea[2, 0] = "O";
+                            if (gameArea[2, 0] == "7")
+                            {
+                                gameArea[2, 0] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[2, 0] = "X";
+                            if (gameArea[2, 0] == "7")
+                            {
+                                gameArea[2, 0] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 8:
                         if (playerNumber == 1)
                         {
-                            gameArea[2, 1] = "O";
+                            if (gameArea[2, 1] == "8")
+                            {
+                                gameArea[2, 1] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[2, 1] = "X";
+                            if (gameArea[2, 1] == "8")
+                            {
+                                gameArea[2, 1] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     case 9:
                         if (playerNumber == 1)
                         {
-                            gameArea[2, 2] = "O";
+                            if (gameArea[2, 2] == "9")
+                            {
+                                gameArea[2, 2] = "O";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         else
                         {
-                            gameArea[2, 2] = "X";
+                            if (gameArea[2, 2] == "9")
+                            {
+                                gameArea[2, 2] = "X";
+                            }
+                            else
+                            {
+                                Console.Write("Number already taken. Select another.");
+                                Console.ReadKey();
+                                UserPlay(gameArea, playerNumber);
+                            }
                         }
                         return gameArea;
                     default:
@@ -177,15 +342,36 @@ namespace TicTacToe
             }
         }
 
-        static void WinEvaluation(int playerNumber)
+        static void WinEvaluation(string[,] gameArea, int playerNumber)
         {
-            //8 kombinacji do wygranej
-            //wejscie to numer gracza
-            //wyjscie to bool z info czy wygral czy nie
+            if (gameArea[0, 0] == gameArea[1, 0] && gameArea[2, 0] == gameArea[1, 0] ||
+                gameArea[0, 1] == gameArea[1, 1] && gameArea[2, 1] == gameArea[1, 1] ||
+                gameArea[0, 2] == gameArea[1, 2] && gameArea[2, 2] == gameArea[1, 2] ||
+                gameArea[0, 0] == gameArea[0, 1] && gameArea[0, 2] == gameArea[0, 1] ||
+                gameArea[1, 0] == gameArea[1, 1] && gameArea[1, 2] == gameArea[1, 1] ||
+                gameArea[2, 0] == gameArea[2, 1] && gameArea[2, 2] == gameArea[2, 1] ||
+                gameArea[0, 0] == gameArea[1, 1] && gameArea[2, 2] == gameArea[1, 1] ||
+                gameArea[0, 2] == gameArea[1, 1] && gameArea[2, 0] == gameArea[1, 1])
+            {
+                ShowGameArea(gameArea);
+                Console.WriteLine($"PLAYER {playerNumber} IS A WINNER");
+                Console.Write("Press an key to continue to reset the game.");
+                Console.ReadKey();
 
+                string[,] gameAreaNew = 
+                {
+                    {"1","2","3"},
+                    {"4","5","6"},
+                    {"7","8","9"}
+                };
+                for (int i = 0; i < gameArea.GetLength(0); i++)
+                {
+                    for (int j = 0; j < gameArea.GetLength(1); j++)
+                    {
+                        gameArea[i, j] = gameAreaNew[i, j];
+                    }
+                }
+            }
         }
-
-
-
     }
 }
